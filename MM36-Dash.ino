dc5b9547e2d9fe5b59eec26fa3f0593f62c28f63
MecMill <thekazanninja@gmail.com>
2025-02-15 18:10:40 -0800

    Mo Big Stuff
    Some clearning up, some CAN prep

--- MM36-Dash.ino
+++ MM36-Dash.ino
@@ -8,28 +8,46 @@
 Adafruit_NeoPixel pixels(NUMPIXELS, DATA_PIN, NEO_GRB + NEO_KHZ800);
 
 // CAN TX-RX Pins
-#define CAN_TX 34
 #define CAN_RX 33
+#define CAN_TX 34
 
-//CAN Recieving frame object
+// Button Pins
+#define Button1Pin 13
+#define Button2Pin 12
+#define Button3Pin 35
+#define Button4Pin 17
+
+//Define 7-segment display segments and their pins on the ESP
+#define GearA 38
+#define GearB 10
+#define GearC 5
+#define GearD 7
+#define GearE 6
+#define GearF 9
+#define GearG 8
+#define GearDP 4
+
+//CAN Recieving and Transmitting frame object
+//Sets sending frame ID
 CanFrame rxFrame;
+int frameID = 0x7F6;
 
 //Defining Tasks
 TaskHandle_t CAN_Task;
 TaskHandle_t Neopixel_Task;
-TaskHandle_t Blink;
+TaskHandle_t Button_Task;
+TaskHandle_t Gear_Task;
 
-//Define 7-segment display segments and their pins
-
-
-//Defines global rpm, clt, gear, etc integers for CAN data
+//Creates global rpm, clt, gear, etc integers for CAN data
 int rpm;
-double clt;
 int gear;
 int neutral;
 int pit;
-int driverSwitch3;
-int driverSwitch4;
+int button1State;
+int button2State;
+int button3State;
+int button4State;
+int clt;
 
 //Defines shfit light rpm switching points
 int shiftRpm1 = 9500;
@@ -39,75 +57,64 @@ int shiftRpm4 = 11000;
 int shiftRpm5 = 11500;
 int shiftRpm6 = 12000;
 int shiftRpm7 = 12500;
+int flashingRPM = 13000;
 
 //Define flashing light bools
-int flashingRPM = 13000;
 bool rpmFlashState = false;
+bool pitFlashState = false;
+bool cltFlashState = false;
 
 //Value at which the "Cold" and "Hot" lights are turned on, in Celsius
 //Value at which light starts flashing
 int coolantCold = 70;
 int coolantHot = 105;
 int coolantFlash = 115;
-bool cltFlashState = false;
 
 //Millis for flashing lights
+//Intervals too
 int previousMillis = 0;
 int previousMillis2 = 0;
 int previousMillis3 = 0;
 const long overheatMillis = 200;
 const long pitMillis = 350;
-const long shiftMillis = 100;
-bool pitFlashState = false;
-
-//OBD TX frame setup
-void sendObdFrame(uint8_t obdId) {
-	CanFrame obdFrame = { 0 };
-	obdFrame.identifier = 0x0F3; // Default OBD2 address;
-	obdFrame.extd = 0;
-	obdFrame.data_length_code = 8;
-	obdFrame.data[0] = 2;
-	obdFrame.data[1] = 1;
-	obdFrame.data[2] = obdId;
-	obdFrame.data[3] = 0xAA;    // Best to use 0xAA (0b10101010) instead of 0
-	obdFrame.data[4] = 0xAA;    // CAN works better this way as it needs
-	obdFrame.data[5] = 0xAA;    // to avoid bit-stuffing
-	obdFrame.data[6] = 0xAA;
-	obdFrame.data[7] = 0xAA;
-  // Accepts both pointers and references 
-  //ESP32Can.writeFrame(obdFrame);  // timeout defaults to 1 ms
-}
+const long shiftMillis = 125;
 
 void setup() {
   //Begin Serial comms
   Serial.begin(115200);
 
+  //Testing Values
   rpm = 8000;
-  clt=130;
+  clt=30;
   pit = 0;
+  neutral = 0;
+
   //Dash button pinModes
-  pinMode(12, INPUT_PULLUP);
-  pinMode(13, INPUT_PULLUP);
+  //Sets pull-up because buttons are pulldown when closed
+  pinMode(Button1Pin, INPUT_PULLUP);
+  pinMode(Button2Pin, INPUT_PULLUP);
+  pinMode(Button3Pin, INPUT_PULLUP);
+  pinMode(Button4Pin, INPUT_PULLUP);
 
   //Gear indicator pinModes
-  // pinMode(4, OUTPUT);
-  // pinMode(5, OUTPUT);
-  // pinMode(6, OUTPUT);
-  // pinMode(7, OUTPUT);
-  // pinMode(8, OUTPUT);
-  // pinMode(9, OUTPUT);
-  // pinMode(10, OUTPUT);
-  // pinMode(38, OUTPUT);
+  pinMode(GearA, OUTPUT);
+  pinMode(GearB, OUTPUT);
+  pinMode(GearC, OUTPUT);
+  pinMode(GearD, OUTPUT);
+  pinMode(GearE, OUTPUT);
+  pinMode(GearF, OUTPUT);
+  pinMode(GearG, OUTPUT);
+  pinMode(GearDP, OUTPUT);
 
-  //Turns gear indicator off
-  // digitalWrite(4,HIGH);
-  // digitalWrite(5,HIGH);
-  // digitalWrite(6,HIGH);
-  // digitalWrite(7,HIGH);
-  // digitalWrite(8,HIGH);
-  // digitalWrite(9,HIGH);
-  // digitalWrite(10,HIGH);
-  // digitalWrite(38,HIGH);
+  //Turns gear indicator off (INVERTED RIGHT NOW, USING COMMONG CATHODE RN)
+  digitalWrite(GearA,LOW);
+  digitalWrite(GearB,LOW);
+  digitalWrite(GearC, LOW);
+  digitalWrite(GearD, LOW);
+  digitalWrite(GearE, LOW);
+  digitalWrite(GearF, LOW);
+  digitalWrite(GearG, LOW);
+  digitalWrite(GearDP, LOW);
 
   //Setting up task for CAN bus stuffz, grabs data and stuff
   xTaskCreatePinnedToCore(
@@ -120,7 +127,7 @@ void setup() {
     0              //Core where the task should run
   );            
 
-  //Settings up task for Shift Light lighting
+  //Settings up task for Lighting 
   xTaskCreatePinnedToCore(
     Light_Task_Code, //Function to implement the task
     "Light Task",    //Name of the task
@@ -131,16 +138,27 @@ void setup() {
     1                   //Core where the task should run
   );
 
-  //Settings up task for Shift Light lighting
-  // xTaskCreatePinnedToCore(
-  //   Gear_Indicator_Code, //Function to implement the task
-  //   "Gear Indicator Task",    //Name of the task
-  //   10000,              //Stack size in words
-  //   NULL,               //Task input parameter
-  //   1,                  //Priority of the task
-  //   &Neopixel_Task,     //Task handle
-  //   1                   //Core where the task should run
-  // );
+  //Settings up task for dash buttons 
+  xTaskCreatePinnedToCore(
+    Button_Task_Code,   //Function to implement the task
+    "Button Task",      //Name of the task
+    10000,              //Stack size in words
+    NULL,               //Task input parameter
+    1,                  //Priority of the task
+    &Button_Task,       //Task handle
+    1                   //Core where the task should run
+  );
+
+  //Settings up task for gear indicator stuff
+  xTaskCreatePinnedToCore(
+    Gear_Indicator_Code, //Function to implement the task
+    "Gear Indicator Task",    //Name of the task
+    10000,              //Stack size in words
+    NULL,               //Task input parameter
+    1,                  //Priority of the task
+    &Gear_Task,         //Task handle
+    1                   //Core where the task should run
+  );
 
   //CAN setup
   ESP32Can.setPins(CAN_TX, CAN_RX);
@@ -168,23 +186,11 @@ void CAN_Task_Code(void *parameter) {
     static uint32_t lastStamp = 0;
     uint32_t currentStamp = millis();
 
-    //Onboard button
-    //int buttonState = digitalRead(35);
-
-    //CAN TX-ing
-    // if(currentStamp - lastStamp > 50) {   // sends OBD2 request every second
-    //   lastStamp = currentStamp;
-    //   sendObdFrame(5); // For coolant temperature
-    // }
-    
-    //Onboard button LED driving, button pressed (0, pulled up) turn the LED on
-    // if(buttonState == 0){
-    //   digitalWrite(14, HIGH);
-    // }
-    // else
-    // {
-    //   digitalWrite(14, LOW);
-    // }
+    //CAN TX-ing, sends dash button states
+    if(currentStamp - lastStamp > 50) {
+      lastStamp = currentStamp;
+      SendButtonCAN();
+    }
 
     //Checks if there are any frames to read
     if(ESP32Can.readFrame(rxFrame, 1000)) {
@@ -226,6 +232,8 @@ void Light_Task_Code(void *parameter2) {
     unsigned long currentMillis = millis();
 
     //Coolant lighting Neopixel
+    //First three "simple" coolant states
+    //Cold, operating temp, hot
     if(clt < coolantCold){
       pixels.setPixelColor(0, pixels.Color(0,0,255));
     }
@@ -235,23 +243,22 @@ void Light_Task_Code(void *parameter2) {
     else if(clt > coolantHot && clt < coolantFlash){
       pixels.setPixelColor(0, pixels.Color(255,0,0));
     }
+
+    //Flashing coolant light for when coolant temp is exceeding coolantFlash threshold
+    //uses currentMillis, previousMillis, overheatMillis to see how much time has 
+    //passed since conditions met, turns coolant light on or off based on that interval
     else if(clt > coolantFlash && cltFlashState && (currentMillis-previousMillis >= overheatMillis)){
         previousMillis = currentMillis;
         cltFlashState = false;
         pixels.setPixelColor(0, pixels.Color(0,0,0));
-        //pixels.show();
     }
     else if(clt > coolantFlash && !cltFlashState && (currentMillis - previousMillis >= overheatMillis)){
         previousMillis = currentMillis;
         cltFlashState = true;
         pixels.setPixelColor(0, pixels.Color(255,0,0));
-        //pixels.show();
     }
-    // else{
-    //   pixels.setPixelColor(0, pixels.Color(255,255,255));
-    // }
 
-    //Shift lights When Pit Limiter is Off
+    //Shift lights When Pit Switch is Off
     //First Light
     if(pit == 0){
 
@@ -310,6 +317,10 @@ void Light_Task_Code(void *parameter2) {
         pixels.setPixelColor(7, pixels.Color(0,0,0));
       }
 
+      //Flashing RPM Light Stuffz
+      //Similar to flashing coolant light
+      //Uses millis and stuffs to find interval of time to turn on or off the
+      //rpm sequence lights
       if(rpm > flashingRPM){
         if(rpmFlashState && (currentMillis-previousMillis3 >= shiftMillis)){
           previousMillis3 = currentMillis;
@@ -321,8 +332,6 @@ void Light_Task_Code(void *parameter2) {
           pixels.setPixelColor(5, pixels.Color(0,0,0));
           pixels.setPixelColor(6, pixels.Color(0,0,0));
           pixels.setPixelColor(7, pixels.Color(0,0,0));
-
-          //pixels.show();
         }
         else if(!rpmFlashState && (currentMillis - previousMillis3 >= shiftMillis)){
           previousMillis3 = currentMillis;
@@ -334,11 +343,14 @@ void Light_Task_Code(void *parameter2) {
           pixels.setPixelColor(5, pixels.Color(100,0,100));
           pixels.setPixelColor(6, pixels.Color(100,0,100));
           pixels.setPixelColor(7, pixels.Color(100,0,100));
-          //pixels.show();
+          //Porple drank
         }
       }
     }
 
+    //Pit Limiter Lights
+    //Alternates between center thingies if pit switch is on
+    //Same millis stuff going on up above
     if(pit == 1){
       if(pitFlashState && (currentMillis-previousMillis2 >= pitMillis)){
         previousMillis2 = currentMillis;
@@ -350,8 +362,7 @@ void Light_Task_Code(void *parameter2) {
         pixels.setPixelColor(5, pixels.Color(0,0,0));
         pixels.setPixelColor(6, pixels.Color(125,100,0));
         pixels.setPixelColor(7, pixels.Color(0,0,0));
-        //pixels.show();
-    }
+      }
       if(!pitFlashState && (currentMillis - previousMillis2 >= pitMillis)){
         previousMillis2 = currentMillis;
         pitFlashState = true;
@@ -362,11 +373,18 @@ void Light_Task_Code(void *parameter2) {
         pixels.setPixelColor(5, pixels.Color(125,100,0));
         pixels.setPixelColor(6, pixels.Color(0,0,0));
         pixels.setPixelColor(7, pixels.Color(125,100,0));
-        //pixels.show();
       }
     }
-      
 
+    //Neutral switch
+    if(neutral == 1){
+      pixels.setPixelColor(8, pixels.Color(0,255,0));
+    }
+    else{
+      pixels.setPixelColor(8, pixels.Color(0,0,0));
+    }
+      
+    //Testing stuff
     if(rpm < 14000){
       rpm = rpm +2;
     }
@@ -380,9 +398,67 @@ void Light_Task_Code(void *parameter2) {
     else{
       clt = 20;
     }
-    
+
+    rpm = 10000;
+    clt =  70;
+    //Set neopixels to set values
+    //Delay to yield to other tasks
     pixels.show();
     vTaskDelay(0.1);
-
   }
+}
+
+void Button_Task_Code(void *parameter){
+  while(true){
+    button1State = digitalRead(Button1Pin);
+    button2State = digitalRead(Button2Pin);
+    button3State = digitalRead(Button3Pin);
+    button4State = digitalRead(Button4Pin);
+
+    vTaskDelay(0.1);
+  }
+}
+
+void SendButtonCAN() {
+	CanFrame obdFrame = { 0 };
+	obdFrame.identifier = frameID;
+	obdFrame.extd = 0;
+	obdFrame.data_length_code = 8;
+	obdFrame.data[0] = 0;
+	obdFrame.data[2] = 0;
+	obdFrame.data[4] = 0; 
+	obdFrame.data[6] = 0;
+
+  //Sets the second byte of the byte pair to 255 depending on button state
+  //Does this for all 4 buttons
+  //Theres probably a better way to do this
+  //Figure the rest out in M1 Tune
+  if(button1State == 0){
+	  obdFrame.data[1] = 0b11111111;
+  }
+  else{
+    obdFrame.data[1] = 0b00000000;
+  }
+
+  if(button2State == 0){
+    obdFrame.data[3] = 0b11111111;
+  }
+  else{
+    obdFrame.data[3] = 0b00000000;
+  }
+	
+  if(button3State == 0){
+	  obdFrame.data[5] = 0b11111111;
+  }
+  else{
+    obdFrame.data[5] = 0b00000000;
+  }
+
+  if(button4State == 0){
+	  obdFrame.data[7] = 0b11111111;
+  }
+  else{
+    obdFrame.data[7] = 0b00000000;
+  }
+  ESP32Can.writeFrame(obdFrame);  // timeout defaults to 1 ms
 }
